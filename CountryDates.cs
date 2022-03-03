e34db5784a972faf4eb199ec3077d7c8f209791e
Kelly Barbosa <kyba@gft.com>
2022-03-03 14:19:42 -0300

    versão 0.0.1

---
+++ CountryDates.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace CountryDates
+{
+    class CountryDates
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var pt = new CultureInfo("pt-PT");
+            var bt = new CultureInfo("pt-BR");
+            var en = new CultureInfo("en-US");
+            var de = new CultureInfo("de-DE");
+            var spa = new CultureInfo("es-ESP");
+            var atual = CultureInfo.CurrentCulture;
+
+            Console.WriteLine(DateTime.Now.ToString("D", es));
+        }
+    }
+}
